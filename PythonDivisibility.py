f3941a54e2182e57175f1ef6e8757989a8d10e94
Rain Erika Angelito <rica_angeli@yahoo.com>
2015-03-18 22:02:05 +0800

    Add PythonDivisibility.py

---
+++ PythonDivisibility.py
@@ -0,0 +1,13 @@
+#Author: Rain Erika H. Angelito
+# CMSC 137 LAB1A
+#Program that ask for a positive integer to list the divisible numbers less
+# than it.
+x=input('Input an integer:  ')
+i=0
+if int(x)>0:
+        while i<int(x):
+                        i=i+1
+                        if(int(x)%i==0 and int(x)!=i):
+                                print(str(i))
+else:
+        print('Please input a positive integer')
